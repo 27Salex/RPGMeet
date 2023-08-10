07a59fcd59e21658a732a83ca8431377014e40fc
roaxen <112213195+roaxen@users.noreply.github.com>
2023-08-10 18:05:53 +0200

    Creacion de repositorio DAL Y Model  - DalUsuario con SelectAll

---
+++ DAL/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackRPG.DAO
+{
+        public class Conexion
+    {
+        private static Conexion instance = null;
+        public SqlConnection Connection { get; }
+
+        private static String dataSource = "Data Source=185.253.153.20,54321;";
+        private static String initialCatalog = "Initial Catalog=ManuelRPG;";
+        private static String user = "User ID=sa;";
+        private static String pass = "Password=123456789;";
+
+        private String cadenaConexion = dataSource + initialCatalog + user + pass;
+        private String connectionString = "Data Source=185.253.153.20,54321;Initial Catalog=ManuelAlvarezEmployees;User ID=sa;Password=123456789;";
+
+
+
+        public Conexion()
+        {
+            Connection = new SqlConnection(cadenaConexion);
+        }
+        public static Conexion Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Conexion();
+                
+            }
+            return instance;
+        }
+    }
+}

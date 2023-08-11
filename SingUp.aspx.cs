ecd7f6781d34b6ff56666678673976e1d634239f
PolSanchezSala <polsansala@gmail.com>
2023-08-11 15:17:56 +0200

    SingUp creation

--- SingUp.aspx.cs
+++ SingUp.aspx.cs
@@ -7,11 +7,16 @@ using System.Web.UI.WebControls;
 
 namespace RPGMeet
 {
-    public partial class CreateUser : System.Web.UI.Page
+    public partial class SingUp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+         //   DalUsuario.cs
         }
     }
 }
--- SingUp.aspx.designer.cs
+++ SingUp.aspx.designer.cs
@@ -11,17 +11,8 @@ namespace RPGMeet
 {
 
 
-    public partial class CreateUser
+    public partial class SingUp
     {
-
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// Control ImageButton1.
@@ -112,5 +103,14 @@ namespace RPGMeet
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }

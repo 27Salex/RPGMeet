42f88a31fa48e694bfaa543adfbc8b16d815b3ec
Alex <alaurencena.edt@gmail.com>
2023-08-16 19:55:32 +0200

    Arreglos en la importacion de bootstrap de master.

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace RPGMeet
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
         /// Control lbl.
         /// </summary>
         /// <remarks>
